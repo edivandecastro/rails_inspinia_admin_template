a2cff5ea706e8e81cc791e0cc98793c835734a4c
Edivan de Castro <edivandecastro@gmail.com>
2022-10-25 10:30:45 -0300

    Instaled administrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    resources :products
+
+    root to: "products#index"
+  end
   resources :products
   get "home/index"
   get "home/minor"
